6026d7267cd50cc0107ca3db366cc7f4396ba3c7
Stephen Wilson <swilson@mit.edu>
2024-03-07 22:52:28 -0500

    Changed navigation bar to use Svelte layout and removed redundancies
    Changed the navigation bar to use Svelte layout component and fixed the bar shifting on hover. Also removed redundancies by moving code from static js, css, and html files to layout component.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<script>
+    import "../style.css";
+    import { page } from '$app/stores';
+    let pages = [
+        {url: "./", title: "Home"},
+        {url: "./resume", title: "Resume"},
+        {url: "./projects", title: "Projects"},
+        {url: "./contact", title: "Contact"},
+    ];
+</script>
+<style>
+    .menu {
+        display: flex;
+        margin-bottom: 1em;
+        border-bottom-width: 1px;
+        border-bottom-style: solid;
+        /* border-bottom-color: oklch(80% 3% 200); */
+        border-bottom-color: var(--border-color);
+        a {
+            flex: 1;
+            text-decoration: none;
+            color: inherit;
+            text-align: center;
+            padding: 0.7em;
+        }
+    }
+
+    .current {
+        border-bottom-width: 0.4em;
+        border-bottom-style: solid;
+        border-bottom-color: oklch(80% 3% 200);
+        padding-bottom: 0.3em;
+    }
+
+    a:not(.current) {
+        &:hover {
+            border-bottom-width: 0.4em;
+            border-bottom-style: solid;
+            border-bottom-color: var(--color-accent);
+            /* background-color: oklch(from var(--color-accent) 95% 5% h); */
+            background-color: color-mix(in oklch, var(--color-accent), canvas 85%);
+            padding-bottom: 0em;
+        }
+    }
+</style>
+
+<nav class="menu">
+	{#each pages as p }
+    <a href={ p.url } class:current={ "." + $page.route.id === p.url } target={ p.url.startsWith("http") ? "_blank" : null }>{ p.title }</a>
+	{/each}
+</nav>
+
+<slot />
--- static/global.js
+++ static/global.js
@@ -1,13 +1,6 @@
-console.log("IT’S ALIVE!");
-
 function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
-
-// navLinks = $$("nav a");
-
-// let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
-// currentLink?.classList.add("current");
 
 let pages = [
 	{url: ".", title: "Home"},
@@ -15,10 +8,6 @@ let pages = [
 	{url: "projects", title: "Projects"},
     {url: "contact", title: "Contact"},
 ];
-
-let nav = document.createElement("nav");
-nav.classList.toggle("menu");
-document.body.prepend(nav);
 
 
 document.body.insertAdjacentHTML("afterbegin",`
@@ -46,29 +35,5 @@ select.addEventListener("input", function (event) {
 if ("colorScheme" in localStorage) {
     setColorScheme(localStorage.colorScheme);
     select.value = localStorage.colorScheme;
-}
-
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    // \/ deleted in step 2.4 of lab 4:
-    // url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
-    // nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>`);
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
-    nav.append(a);
-
-    a.classList.toggle("current", a.host === location.host && a.pathname === location.pathname);
-    if (a.host !== location.host && a.pathname === location.pathname) {
-        a.target = "_blank";
-    }
-    // a.target.toggle("_blank", a.host !== location.host && a.pathname !== location.pathname);
-    
-    // if (a.host === location.host && a.pathname === location.pathname) {
-    //     a.classList.add("current");
-    // }
-
 }
 
